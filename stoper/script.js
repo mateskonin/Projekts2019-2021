ccd87c842ba37f0f185c7b04b721783fadd11961
Dawid <mateskonin@interia.eu>
2022-03-24 11:39:45 +0100

    add change colors in js

--- stoper/script.js
+++ stoper/script.js
@@ -4,11 +4,14 @@ const stopBtn = document.querySelector('.stop')
 const resetBtn = document.querySelector('.reset')
 const historyBtn = document.querySelector('.history')
 const infoBtn = document.querySelector('.info')
+const colorBtn = document.querySelector('.colors')
 const closeBtn = document.querySelector('.close')
 const stopWatch = document.querySelector('.stopwatch')
 const time = document.querySelector('.time')
 const timeList = document.querySelector('.time-list')
 const modalShadow = document.querySelector('.modal-shadow')
+const root = document.querySelector(':root')
+const wrapper = document.querySelector('.wrapper')
 
 let countTime;
 let seconds = 0;
@@ -59,7 +62,31 @@ const handleReset = () => {
     timeList.textContent = ""
 }
 
+const showHistory = () => {
+    timeList.classList.toggle("show")
+}
+
+const showInfo = () => {
+    modalShadow.style.display = "block"
+    modalShadow.classList.toggle("modal-animation")
+    // root.style.setProperty('--first-color', '#117e11')
+    // root.classList.toggle("light")
+}
+
+const closeInfo = () => {
+    modalShadow.style.display = "none"
+    modalShadow.classList.toggle("modal-animation")
+}
+const changeColor = () => {
+    root.classList.toggle("light")
+    wrapper.classList.toggle("light")
+}
+
 startBtn.addEventListener("click", handleStart)
 pauseBtn.addEventListener("click", handlePause)
 stopBtn.addEventListener("click", handleStop)
 resetBtn.addEventListener("click", handleReset)
+historyBtn.addEventListener("click", showHistory)
+infoBtn.addEventListener("click", showInfo)
+colorBtn.addEventListener("click", changeColor)
+closeBtn.addEventListener("click", closeInfo)
